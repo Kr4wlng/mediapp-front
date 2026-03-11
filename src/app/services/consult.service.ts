10ff43bc451a248fa11d144182182db36001b8f7
adurand <adurand@sintad.pe>
2026-03-10 20:06:42 -0500

    Componente Consult: Save

--- src/app/services/consult.service.ts
+++ src/app/services/consult.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { ConsultListExamDTOI } from '../models/consultListExamDTOI';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,9 @@ export class ConsultService {
 
   constructor(private http: HttpClient){ }
   
-  /* saveTransactional(dto: ???){
-  
-  } */
+  saveTransactional(dto: ConsultListExamDTOI){
+    return this.http.post(this.url, dto);
+  }
 
   
 
