e251717883375d0d9b4a7c5d88ee8fd10fc60121
adurand <adurand@sintad.pe>
2026-03-20 16:21:07 -0500

    Componente Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MatCard } from "@angular/material/card";
+import { MaterialModule } from '../material/material.module';
+import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MaterialModule, RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+  username: string;
+  password: string;
+
+  constructor(
+    private loginService: LoginService
+  ){}
+
+  login(){
+    this.loginService.login(this.username, this.password).subscribe(data => {
+      // console.log(data);
+      sessionStorage.setItem(environment.TOKEN_NAME, data.access_token);
+
+      // let token: string = sessionStorage.getItem(environment.TOKEN_NAME);
+    });
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
 
     HOST: 'http://localhost:8080',
-    RETRY: 2
+    RETRY: 2,
+    TOKEN_NAME: 'access_token'
 
 };
